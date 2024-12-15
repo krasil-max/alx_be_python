48d7f7a4307e9f8e76c1f2505253a6c2d5effa84
krasil-max <krasilsapphire@gmail.com>
2024-12-16 01:09:53 +0300

    addition of basic_operations.py file

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,9 @@
+xnumber1 = 10
+number2 = 5
+# Addition of number1 and number2
+print(f'Addition  of {number1} and {number2} is {number1 + number2}')
+# Subtraction of number1 and number2
+print(f'Subtraction  of {number1} and {number2} is {number1 - number2}')
+# Multiplication of number1 and number2
+print(f'Multiplication  of {number1} and {number2} is {number1 * number2}')
+ 
