92c35f925a6296e1789be07a1cb731b85b425b82
krasil-max <krasilsapphire@gmail.com>
2024-12-23 01:30:18 +0300

    multiplication table file

--- control-flow/match_case_calculator.py
+++ control-flow/match_case_calculator.py
@@ -13,9 +13,5 @@ match operation :
         print(f'The result is {result}.')
     case "/":
         result = num1 / num2
-        if num2 == 0:
-            print('invalid num2. Please enter again')
-        else:
-            print(f'The result is {result}.')
     case _:
         print("Invalid operation! Please choose one of +, -, *, or /.")
