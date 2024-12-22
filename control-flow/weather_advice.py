3f278827d2629625cf4aacdf97932c84f57021ef
krasil-max <krasilsapphire@gmail.com>
2024-12-23 00:33:14 +0300

    changed the variable from conditions-weather

--- control-flow/weather_advice.py
+++ control-flow/weather_advice.py
@@ -1,9 +1,9 @@
-conditions = input("What's the weather like today? (sunny/rainy/cold):")
-if conditions == 'sunny':
+weather = input("What's the weather like today? (sunny/rainy/cold):")
+if weather == 'sunny':
     print('Wear a t-shirt and sunglasses.')
-elif conditions == 'rainy':
+elif weather == 'rainy':
     print("Don't forget your umbrella and a raincoat.")
-elif conditions == 'cold':
+elif weather == 'cold':
     print("Make sure to wear a warm coat and a scarf.")
 else:
     print("Sorry, I don't have recommendations for this weather.")
