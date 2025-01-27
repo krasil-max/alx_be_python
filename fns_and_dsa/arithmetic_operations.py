b425146f1509b11906b8773c72078a955f2e2a1b
krasil-max <krasilsapphire@gmail.com>
2025-01-27 16:09:21 +0300

    corrected arithmetic operation

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -1,18 +1,17 @@
-def perform_operation (num1,num2,operation):
+def perform_operation(num1, num2, operation):
+   if operation == 'add':
+      return num1 + num2
+   elif operation == 'subtract':
+      return num1 - num2
+   elif operation == 'multiply':
+      return num1 * num2
+   elif operation == 'divide':
+      if num2 == 0:
+         raise ZeroDivisionError("Cannot divide by zero")
+      return num1 / num2
+   else:
+      raise ValueError("Invalid operation. Valid operations are 'add', 'subtract', 'multiply', or 'divide'.")
 
-    if operation == 'add':
-        return num1 + num2
-    elif operation == 'subtract':
-        return num1 - num2
-    elif operation == "multiply":
-        return num1 * num2
-    elif operation == 'divide':
-        if num2 == 0:
-            return ('num1 not divisible by num2')
-        return num1 / num2
-    else:
-        return 'invalid input'
-            
 from arithmetic_operations import perform_operation
 
 def main():
