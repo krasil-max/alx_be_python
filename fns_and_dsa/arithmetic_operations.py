56f425b1f603e67f7b760da42183e09211266c1f
krasil-max <krasilsapphire@gmail.com>
2025-01-27 14:57:06 +0300

    arithmetic calculations

---
+++ fns_and_dsa/arithmetic_operations.py
@@ -0,0 +1,16 @@
+def perform_operations(num1:float ,num2:float ,operation:str ):
+    
+    if operation == 'add':
+        return num1 + num2
+    elif operation == 'subtract':
+        return num1 - num2
+    elif operation == "multiply":
+        return num1 * num2
+    elif operation == 'divide':
+        if num2 == 0:
+            return ('num1 not divisible by num2')
+        return num1 / num2
+    else:
+        return 'invalid input'
+            
+print(perform_operations(423.5,87.8,'divide'))
