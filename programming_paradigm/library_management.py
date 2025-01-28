583512f7e6f46462296d633a277d1b2dc6479167
krasil-max <krasilsapphire@gmail.com>
2025-01-28 16:09:20 +0300

    library management

---
+++ programming_paradigm/library_management.py
@@ -0,0 +1,36 @@
+
+class Book:
+    def __init__(self, title, author):
+        self.title = title
+        self.author = author
+        self._is_checked_out = False
+
+class Library:
+    def __init__(self):
+        self._books = []
+
+    def add_book(self, book):
+        self._books.append(book)
+
+    def check_out_book(self, title):
+        for book in self._books:
+            if book.title == title and not book._is_checked_out:
+                book._is_checked_out = True
+                return True
+        return False
+    def return_book(self):
+        self._is_checked_out = False
+        
+    def return_book(self, title):
+        for book in self._books:
+            if book.title == title and book._is_checked_out:
+                book._is_checked_out = False
+                return True
+        return False
+
+    def list_available_books(self):
+        available_books = []
+        for book in self._books:
+            if not book._is_checked_out:
+                available_books.append(book.title)
+        return available_books
